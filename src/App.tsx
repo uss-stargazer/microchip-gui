c8bebad3b384a1b4ed2e12e8f711398f23db1503
anonymous <anonymous>
2025-10-15 18:40:06 -0400

    feat(tabs): Add TabBarButtons (makeTabBarButton.tsx and tabBarButtons in App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { ThemeProvider } from "@emotion/react";
 import { createTheme, CssBaseline } from "@mui/material";
-import TabFrame from "./components/TabFrame";
+import type { ReactElement } from "react";
+import { MdInfo, MdSettings } from "react-icons/md";
+
+import TabFrame, { type Tab } from "./components/TabFrame";
+
+import Home from "./tabs/Home";
+import About from "./tabs/About";
+import Settings from "./tabs/Settings";
+import Workspace from "./tabs/Workspace";
+import makeTabBarButton from "./modules/makeTabBarButton";
 
 const theme = createTheme({
   palette: {
@@ -19,11 +28,20 @@ const theme = createTheme({
   },
 });
 
+const tabBarButtons: { icon: ReactElement; tab: Tab }[] = [
+  { icon: <MdInfo />, tab: { id: "About", element: <About /> } },
+  { icon: <MdSettings />, tab: { id: "Settings", element: <Settings /> } },
+];
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <TabFrame barButtons={[]} />
+      <TabFrame
+        barButtons={tabBarButtons.map(({ icon, tab }) =>
+          makeTabBarButton(icon, tab)
+        )}
+      />
     </ThemeProvider>
   );
 }
---
+++ src/modules/makeTabBarButton.tsx
@@ -0,0 +1,21 @@
+import type { ReactElement } from "react";
+import type { Tab, TabBarButton } from "../components/TabFrame";
+import { Button, IconButton } from "@mui/material";
+
+function makeTabBarButton(icon: ReactElement, tab: Tab): TabBarButton {
+  return ({ addTab }) => (
+    <Button
+      onClick={() => addTab(tab)}
+      color="secondary"
+      sx={{
+        p: 0,
+        borderRadius: 0,
+        border: "1px solid rgba(0,0,0,0)",
+      }}
+    >
+      <IconButton size="small">{icon}</IconButton>
+    </Button>
+  );
+}
+
+export default makeTabBarButton;

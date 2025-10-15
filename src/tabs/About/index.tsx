9fc12a442295cba904a2c6144c0642b226c0d4c0
anonymous <anonymous>
2025-10-15 19:25:20 -0400

    wording(about): Update About tab text

--- src/tabs/About/index.tsx
+++ src/tabs/About/index.tsx
@@ -1,7 +1,26 @@
+import { Typography } from "@mui/material";
 import Tab from "../../components/Tab";
 
 function About() {
-  return <Tab>About</Tab>;
+  return (
+    <Tab
+      flex="column"
+      sx={{ margin: "auto", maxWidth: "800px", p: "1rem", gap: "1rem" }}
+    >
+      <hr />
+      <Typography variant="h6">
+        <span style={{ fontWeight: "lighter" }}>Microchip GUI</span> is a
+        interface for displaying, <br />
+        testing, and exporting nested electronics circuits.
+      </Typography>
+      <hr />
+      <Typography>
+        Microchip uses a functional representation of circuits where a component
+        is a single entity with its own internal state and that can be
+        implemented in the state of any other component. This allows modularity
+      </Typography>
+    </Tab>
+  );
 }
 
 export default About;

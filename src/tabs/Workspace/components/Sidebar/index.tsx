9e006f9517f10d20231c74885d6907be5d6e46c8
anonymous <anonymous>
2025-10-15 19:22:22 -0400

    feat(workspace): Flesh out empty Sidebar

--- src/tabs/Workspace/components/Sidebar/index.tsx
+++ src/tabs/Workspace/components/Sidebar/index.tsx
@@ -1,7 +1,38 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { MdSettingsApplications } from "react-icons/md";
+
+function WorkspaceSettings() {
+  return (
+    <Box
+      bgcolor="grey.900"
+      sx={{
+        borderLeftWidth: "5px",
+        borderLeftStyle: "solid",
+        borderLeftColor: "grey.800",
+        display: "flex",
+        flexDirection: "column",
+        p: "1.5rem",
+      }}
+    >
+      <Typography>Workspace Settings</Typography>
+    </Box>
+  );
+}
 
 function Sidebar() {
-  return <Box>Sidebar</Box>;
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
+  return (
+    <Box display="flex">
+      {isOpen && <WorkspaceSettings />}
+      <Box bgcolor="grey.800">
+        <IconButton onClick={() => setIsOpen(!isOpen)}>
+          <MdSettingsApplications />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 }
 
 export default Sidebar;

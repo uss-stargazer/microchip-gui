93f5508a60edd11116b04fb81a8ce21be16615ff
anonymous <anonymous>
2025-10-15 20:31:31 -0400

    wip Network jazz!

--- src/tabs/Workspace/index.tsx
+++ src/tabs/Workspace/index.tsx
@@ -1,11 +1,20 @@
 import { Box } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import TabLayout from "../../components/TabLayout";
+import parseMicrochipState from "./modules/parseMicrochipState";
+import type { MicrochipState } from "microchip-dsl";
+import useVisNetwork from "../../modules/useVisNetwork";
 
-function Workspace() {
+function Workspace({ microchipState }: { microchipState: MicrochipState }) {
+  const { networkRef, network } = useVisNetwork(
+    parseMicrochipState(microchipState)
+  );
+
   return (
     <TabLayout flex="row">
-      <Box sx={{ flexGrow: 1 }}></Box>
+      <Box sx={{ flexGrow: 1, height: "100%", bgcolor: "blue" }}>
+        <div ref={networkRef} />
+      </Box>
       <Sidebar />
     </TabLayout>
   );
